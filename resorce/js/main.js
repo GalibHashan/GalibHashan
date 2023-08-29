c7b3c3cdadd1cb2ab0ee37f81df68ce9b8d44c33
Galibhashan <galibhashan5413@gmail.com>
2023-08-29 15:10:20 +0600

    first commit

---
+++ resorce/js/main.js
@@ -0,0 +1,10 @@
+// NAVEGATION
+function openMenu(){
+    document.getElementById("menu-contaner").style.width="100%";
+}
+function closeMenu() {
+    document.getElementById("menu-contaner").style.width = "0";
+}
+
+// MIXER
+var mixer = mixitup('.work-grid');
